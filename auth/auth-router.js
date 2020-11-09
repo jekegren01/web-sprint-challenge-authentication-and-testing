c54f428b0e40907ffe1a8843aa2f9ac2d8958d57
Jacob Ekegren <JacobEkegren@MacBook-Pro.local>
2020-11-09 14:55:17 -0800

    cleaning up code

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -7,13 +7,22 @@ router.post('/register', async (req, res) => {
   
     try {
       const {username, password} = req.body;
-      if(!username) return res.status(400).json({message: "Username and password required"});
-      if(!password) return res.status(400).json({message: "Username and password required"});
+      if(!username) 
+        return 
+          res.status(400).json({message: "Username and password required"});
+      if(!password) 
+        return 
+          res.status(400).json({message: "Username and password required"});
+
      const userExists = await authModel.getByUsername(username);
-     if(userExists) return res.status(400).json({message: "Username taken"});
+      if(userExists) 
+        return 
+          res.status(400).json({message: "Username taken"});
 
       const user = await authModel.createUser(username, password);
-     if(user) return res.status(201).json(user);
+     if(user) 
+      return 
+        res.status(201).json(user);
    }catch(err){
      console.log(err);
      res.status(500).json({message: "A server error occurred"});
@@ -23,14 +32,20 @@ router.post('/register', async (req, res) => {
  
  router.post('/login', async (req, res) => {
   const {username, password} = req.body;
-  if(!username || !password) return res.status(400).json({message: "Username and password required"});
+  if(!username || !password) 
+    return 
+      res.status(400).json({message: "Username and password required"});
 
    const user = await authModel.getByUsername(username);
-  if(!user) return res.status(401).json({message: "Incorrect username or password"});
-  if(!bcrypt.compareSync(password, user.password)) return res.status(401).json({message: "Incorrect username or password"});
+  if(!user) 
+    return 
+      res.status(401).json({message: "Incorrect username or password"});
+  if(!bcrypt.compareSync(password, user.password)) 
+    return 
+      res.status(401).json({message: "Incorrect username or password"});
   const tokenPayload = {subject: user.id, userame: user.username};
-   const token = jwt.sign(tokenPayload, process.env.JWT_SECRET, {expiresIn: "1d"});
-   res.status(200).json({token});
+  const token = jwt.sign(tokenPayload, process.env.JWT_SECRET, {expiresIn: "1d"});
+  res.status(200).json({token});
 });
 
 module.exports = router;
